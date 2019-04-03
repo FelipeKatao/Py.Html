c277e8d14921c04ee82ed4978db817e1c109e447
Felipe Catão do Nascimento <34559081+FelipeKatao@users.noreply.github.com>
2019-04-03 16:00:50 -0300

    Base de dados para o projeto

---
+++ Framework/PyHtml.py
@@ -0,0 +1,10 @@
+
+#Create, edit and read Html files 
+class HtmlPy():
+  pass
+#Create, edit and read CSS files 
+class CssPy():
+  pass
+#Class for search data in HTML files and create analitic data.
+class SearchData():
+  pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from Framework import PyHtml
+#Somente para testar
+vr2=PyHtml.HtmlClass()
+vr2.DefinePath("Local","LocalPt")
