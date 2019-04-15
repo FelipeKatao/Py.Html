8393cb18a45dce6bc7172b6a5a04e94a44bf5348
FelipeKatao <cataofelipe@gmail.com>
2019-04-15 02:55:29 -0300

    Model example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Framework import PyHtml
-#Template para testes
+#Template for testes
 #It's system work unique function with python tecnology
 #Create HTML and CSS files with less lines in your Body.
 #You no need create your web Project with hard dificult.
@@ -10,5 +10,7 @@ from Framework import PyHtml
 #code exemple:
 vr2= PyHtml.HtmlPy()
 vr2.CreateNewHtml("about","Project",1)
+vr2.CreateNewHtml("index","Project",1)
 vr2.LinkCss(R"Project\style.css",R"Project\about.html")
+vr2.LinkCss(R"Project\style.css",R"Project\index.html")
 #Test
