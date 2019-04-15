c3ef4cf2ecd26b03af473eaee8b875e01b8e5d35
FelipeKatao <cataofelipe@gmail.com>
2019-04-15 02:44:54 -0300

    New functions and coment code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from Framework import PyHtml
-#Somente para testar
+#Template para testes
+#It's system work unique function with python tecnology
+#Create HTML and CSS files with less lines in your Body.
+#You no need create your web Project with hard dificult.
+#Uses .CreateNewHtml() <--Where  create  one new HTML code.
+#Its very easy and cool.
+#Powered by Felipe Katao Tecnology.
+
+#code exemple:
 vr2= PyHtml.HtmlPy()
-vr2.CreateNewHtml("index","Project",1)
-vr2.LinkCss(R"Project\style.css",R"Project\index.html")
+vr2.CreateNewHtml("about","Project",1)
+vr2.LinkCss(R"Project\style.css",R"Project\about.html")
 #Test
