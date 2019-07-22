6dcef03c0ac54981713e1a937821a6e885d5c08f
William Sousa <william@willss.com.br>
2019-07-22 11:27:18 -0300

    Slider Update Code
    News Updates

--- Framework/Components/js/slider.js
+++ Framework/Components/js/slider.js
@@ -1,16 +1,33 @@
+/*////////////////////////////////
+///// SLIDER ANIMATION //////////
+////////////////////////////////*/
+
+var query = document.querySelectorAll('[slider-py] > p > img');
+var elementos = Array.from(query);
+var tamanho = elementos.length;
+var transicao = 500;
 var intervalo = 4000;
+var intervaloAtual = 4000;
+var intervaloControle = 0;
 
-function slide1(){
-	document.getElementById("banner").src = "images/slide1.jpg";
-	setTimeout("slide2()", intervalo);
+function exibir(element, indice, array, exibir){
+
+	if(intervaloControle == 0){
+		setTimeout(() => $(elementos[indice]).show(transicao), transicao);
+		setTimeout(() => $(elementos[indice]).fadeOut(transicao),intervalo);
+		intervaloAtual = intervalo+transicao; 
+		intervaloControle = 1;
+	}else{
+		setTimeout(() => $(elementos[indice]).fadeIn(transicao), intervaloAtual);
+		intervaloAtual += (intervalo+transicao);
+		setTimeout(() => $(elementos[indice]).fadeOut(transicao), intervaloAtual);
+	}
+	if(indice == (tamanho - 1)){
+		setTimeout(startSlide, 0);
+	}
+	
 }
 
-function slide2(){
-	document.getElementById("banner").src = "images/slide2.jpg";
-	setTimeout("slide3()", intervalo);
-}
-
-function slide3(){
-	document.getElementById("banner").src = "images/slide3.jpg";
-	setTimeout("slide1()", intervalo);
+function startSlide(){
+	elementos.forEach(exibir);
 }
