cc94e2dffef4c35496a2bf0b45219e10daf9425d
Felipe Catão do Nascimento <34559081+FelipeKatao@users.noreply.github.com>
2019-08-03 09:39:47 -0300

    React Model suport

---
+++ Framework/ReactModel/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Menu from "./menu.js"
+
+class App extends Component {
+  render() {
+   //Create here the Menu component//
+  }
+}
+
+export default App;
