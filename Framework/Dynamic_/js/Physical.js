fcf5605a5bcfea3c71943517797b8631ab2a5b4b
FelipeKatao <cataofelipe@gmail.com>
2019-10-11 23:57:39 -0300

    upt

--- Framework/Dynamic_/js/Physical.js
+++ Framework/Dynamic_/js/Physical.js
@@ -1,7 +1,7 @@
-function myMove(target) {
+function gravity(target,force) {
     var elem = document.getElementById(target);   
     var pos = 0;
-    var id = setInterval(frame, 13);
+    var id = setInterval(frame,force);
     function frame() {
         pos++; 
         elem.style.top = pos + "px"; 
