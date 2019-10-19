562d9a959c3c5b2e1b5b2f9085181de20e500060
FelipeKatao <cataofelipe@gmail.com>
2019-10-19 01:02:05 -0300

    move elements via Keyboard

--- Framework/Dynamic_/js/Controller.js
+++ Framework/Dynamic_/js/Controller.js
@@ -1,3 +1,30 @@
-function moveToMouse(){
-    
+var targetSelect="";
+var speed=0;
+
+document.addEventListener('keydown',(event)=>{
+    const keyName= event.key;
+    let targetMove= document.getElementById(targetSelect);
+
+    if(keyName=="ArrowDown" || keyName=="s"){
+        targetMove.style.down=speed+"px";
+        console.log("down"+speed);
+
+    }
+    if(keyName=="ArrowLeft" || keyName=="d"){
+        targetMove.style.left=speed+"px";
+        console.log("left");
+    }
+    if(keyName=="ArrowRight" || keyName=="a"){
+        targetMove.style.right=speed+"px";
+        console.log("right");
+    }
+    if(keyName=="ArrowUp" || keyName=="w"){
+        targetMove.style.top=speed+"px";
+        console.log("Up");
+    }
+})
+
+function selectTarget(target,speedTarget){
+    targetSelect=target;
+    speed=speedTarget;
 }
